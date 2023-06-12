e7a92b3f1558b8aef10521949d966db53ca23420
iori <134770602+iorise@users.noreply.github.com>
2023-06-12 21:58:01 +0700

    making functionality

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-const Player = ( name, mark, className ) => {
-  return { name, mark,className }
+const Player = (name, mark, className) => {
+  return {name, mark, className}
+}
+
+const mainGame = () => {
+  const player1 = Player("Player X", "X", "field-x")
+  const player2 = Player("Player O", "O", "field-o")
+
+  const fieldElements = document.querySelectorAll(".field")
+  const messageElement = document.getElementById("message")
+  const restartBtn = document.getElementById("restartBtn")
+
+  let currentPlayer = player1
+  let isOver = false
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1
+    messageElement = `${currentPlayer}'s Turn`
+  }
 }
